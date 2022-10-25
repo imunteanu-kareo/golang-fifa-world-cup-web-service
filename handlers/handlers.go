0a7c10863447eef4a858aafb9900629810bf06b7
Ivan Munteanu <ivan@munteanu.co>
2022-10-25 17:19:55 -0400

    add winners handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,17 +1,33 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-
+	res.WriteHeader(http.StatusNoContent)
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
-
+	year := req.URL.Query().Get("year")
+	res.Header().Set("Content-Type", "application/json")
+	if year == "" {
+		allWinners, err := data.ListAllJSON()
+		if err != nil {
+			res.Write(allWinners)
+		}
+		res.WriteHeader(http.StatusInternalServerError)
+	} else {
+		yearWinners, err := data.ListAllByYear(year)
+		if err != nil {
+			res.Write(yearWinners)
+		}
+		res.WriteHeader(http.StatusInternalServerError)
+	}
+	return
 }
 
 // AddNewWinner adds new winner to the list
